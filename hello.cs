9e9c53363499e32963820a46ea0d86fd42cb824c
麻油 <69881534+ma-you@users.noreply.github.com>
2020-08-19 11:26:13 +0800

    Seconde

--- hello.cs
+++ hello.cs
@@ -3,7 +3,7 @@ using System;
 namespace Github{
     class Program{
         static void Main(string[] args){
-            System.Console.Wtriteline("miaomiao");
+            System.Console.Writeline("miaomiao");
         }
     }
 }
